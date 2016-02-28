289cd4a766a5ba10119f8f4266ec7f61fd6cd40f
TheMoniulla <skrobis.monika@gmail.com>
2016-02-28 21:53:17 +0100

    name of week

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def price(value)
     number_to_currency(value, unit: '', delimiter: '')
   end
+
+  def name_of_week(date)
+    "Week no. #{date.strftime("%U")}"
+  end
 end
