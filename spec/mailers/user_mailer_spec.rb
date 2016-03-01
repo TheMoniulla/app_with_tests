2929b94354777f09df2ad44b52d8b8c9fe396f9a
TheMoniulla <skrobis.monika@gmail.com>
2016-03-01 11:01:39 +0100

    user_mailer_spec.rb

---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe UserMailer do
+  describe 'info_email' do
+    let(:user) { create(:user, email: 'user@user.com' ) }
+    let(:mail) { UserMailer.info_email(user) }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Weekly report of your expenses')
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq(['admin@user.com'])
+    end
+
+    it 'assigns @email' do
+      expect(mail.body.encoded).to match(user.email)
+    end
+  end
+end
