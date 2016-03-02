029bbf4b352af369610bdb0ebac3fd6724c7bdc5
TheMoniulla <skrobis.monika@gmail.com>
2016-03-02 14:38:42 +0100

    weekly expenses for group

--- app/controllers/user/reports_controller.rb
+++ app/controllers/user/reports_controller.rb
@@ -4,4 +4,6 @@ class User::ReportsController < User::UserController
   expose(:q) { Expense.ransack(params[:q]) }
   expose(:expenses) { q.result.for_group(group).includes(:user) }
   expose(:expenses_by_group) { expenses.group_by(&:expenses_group) }
+  expose(:expenses_for_week) { expenses.for_week(date) }
+  expose(:date) { params[:date] ? Date.parse(params[:date]) : Date.today }
 end
