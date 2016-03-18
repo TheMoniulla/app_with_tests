04c336f279905f16b0d6669383fb4a86e4049248
TheMoniulla <skrobis.monika@gmail.com>
2016-03-18 14:55:33 +0100

    typo

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "user_#{n}@app_with_tests.com" }
     password 'secret33'
-    password_conirmation 'secret33'
+    password_confirmation 'secret33'
   end
 end
