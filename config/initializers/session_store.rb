99832a2b9d156387ecdf1945adf392cac7cebf6a
TheMoniulla <skrobis.monika@gmail.com>
2016-03-23 15:58:39 +0100

    gem activerecord-session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :active_record_store, key: '_Application_session'
+Rails.application.config.session_store :active_record_store, key: '_app_with_tests_session'
