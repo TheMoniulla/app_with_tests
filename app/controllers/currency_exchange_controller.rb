2a31571957ab302d879c760db81fd086a283f320
TheMoniulla <skrobis.monika@gmail.com>
2016-03-29 08:14:01 +0200

    one form for saving set in the DB & displaying the currency

--- app/controllers/currency_exchange_controller.rb
+++ app/controllers/currency_exchange_controller.rb
@@ -2,13 +2,24 @@ class CurrencyExchangeController < ApplicationController
   expose(:money_pln) { Money.new(1_00, 'PLN') }
   expose(:money_eur) { Money.new(1_00, 'EUR') }
   expose(:money_usd) { Money.new(1_00, 'USD') }
-  expose(:currencies) { Currency.all }
+  expose(:currencies_for_select) { Currency.all.map { |c| [c.name, c.name] } }
+
   expose(:value) { params[:value] || 0 }
   expose(:base_currency) { params[:base_currency] || 'PLN' }
   expose(:new_currency) { params[:new_currency] || 'PLN' }
+  expose(:new_currency_exchange_setting) { CurrencyExchangeSetting.new(value: 0,
+                                                                       base_currency: 'PLN',
+                                                                       new_currency: 'PLN') }
   expose(:converted_value) do
     m = Money.new(value, base_currency)
     m.exchange_to(new_currency).to_f * 100
   end
-  expose(:currency_exchange_settings) { CurrencyExchangeSetting.all}
+  expose(:currency_exchange_settings) { CurrencyExchangeSetting.all }
+
+  def index
+    respond_to do |format|
+      format.html { render 'index' }
+      format.json { render json: {converted_value: converted_value} }
+    end
+  end
 end
